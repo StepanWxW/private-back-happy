a607fe2b4d1e541146444f8e326cf976718d2b13
Polyakov Stepan <stepan@bubuka.info>
2023-10-04 16:22:26 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.10"
+    id("io.ktor.plugin") version "2.3.4"
+}
+
+group = "com.food"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.food.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-serialization-jackson-jvm")
+    implementation("io.ktor:ktor-server-cio-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.slf4j:slf4j-api:$1.7.36")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.food.ktor-sample"
